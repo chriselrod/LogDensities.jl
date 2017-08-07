fa6032859f1a3f63b955385279f7efbc273bbd54
Chris Elrod <elrodc@gmail.com>
2017-08-07 05:23:49 -0500

    Updating API.

--- src/model.jl
+++ src/model.jl
@@ -1,17 +1,23 @@
 
 
-struct Model{p, q <: QuadratureRule, P <: parameters{Float64}, B <: GridBuild}
-  Grid::GridContainer{p,q}
+struct Model{q <: QuadratureRule, P <: parameters{Float64}, B <: GridBuild}
+  Grid::GridVessel{q,B}
   Θ::P
   UA::UnionAll
 end
 
 
-function Model(::Type{P}; l::Int = 6, ::Type{B} = AdaptiveBuild{GenzKeister}) where {P <: parameters, q, B <: GridBuild}
+function Model(::Type{P}, ::Type{B}) where {P <: parameters, q, B <: AdaptiveBuild{q}}
+  Θ = construct(P{Float64})
+  p = type_length(P)
+  Grid = GridVessel(q, B, Tuple{Int,Int,Int}, p)
+  Model{q, P, B}(Grid, Θ, P)
+end
+function Model(::Type{P}, ::Type{B} = AdaptiveBuild{GenzKeister}) where {P <: parameters, q, B <: GridBuild}
   Θ = construct(P{Float64})
   p = length(Θ)
-  Grid = GridContainer(p, l, q, seq)
-  Model{p, q, P, B}(Grid, Θ, P)
+  Grid = GridVessel(q, B, Tuple{Int,Vector{Int}}, p)
+  Model{q, P, B}(Grid, Θ, P)
 end
 
 function Base.show(io::IO, ::MIME"text/plain", m::Model)
